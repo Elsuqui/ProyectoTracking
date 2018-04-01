158c1900e41cb533b3e4a29b159a4950c5720e86
Gorky Suquinagua <gsuquinagua@hotmail.es>
2018-04-01 00:11:50 -0500

    Preparando plantilla y agregando semantic ui

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,49 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="ui one column stackable center aligned page grid">
+        <div class="column twelve wide">
+            <div class="ui raised segment">
+            <img class="ui centered small circular image" src="{{ asset('images/user_image.png') }}">
+                
+                <h4 class="ui horizontal divider header">
+                <i class="user icon"></i>{{ __('Iniciar Sesión') }}
+                </h4>
+            <form method="POST" action="{{ route('login') }}" class="ui form">
+                @csrf
+            <div class="field {{ $errors->has('email') ? 'error' : '' }}">
+                    <label for="email">{{ __('Correo Electrónico') }}</label>
+                    <input id="email" type="email" name="email" value="{{ old('email') }}" required autofocus>
+                @if ($errors->has('email'))
+                    <div class="ui error message">
+                        <div class="header">Error de Mail</div>
+                        <p>{{$errors->first('email')}}</p>
+                    </div>
+                @endif
+            </div>
+            <div class="field {{ $errors->has('password') ? 'error' : '' }}">
+                <label for="password" class="">{{ __('Contraseña') }}</label>
+                <input id="password" type="password" class="" name="password" required>
+                @if ($errors->has('password'))
+                <div class="ui error message">
+                    <div class="header">Error de Password</div>
+                    <p>{{$errors->first('password')}}</p>
+                </div>
+                @endif
+            </div>
+            <div class="field">
+                    <div class="ui checkbox">
+                      <input type="checkbox" tabindex="0" class="hidden" name="remember" {{ old('remember') ? 'checked' : '' }}>
+                      <label>{{ __('Remember Me') }}</label>
+                    </div>
+            </div>
+                    <button class="ui green button" type="submit">{{ __('Iniciar') }}</button>
+                    <a class="ui brown button" href="{{ route('password.request') }}">
+                            {{ __('Olvidaste la contraseña?') }}
+                    </a>
+            </form>
+            </div>
+        </div>
+     </div>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'BusTracking') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/semantic.min.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <!-- Styles -->
+    <link href="{{ asset('css/semantic.min.css') }}" rel="stylesheet">
+</head>
+<body>
+
+    <div class="ui fluid container">
+        <div class="ui attached stackable menu">
+            <div class="header item">
+                <img src="{{ asset('images/logo.png') }}" alt="Incio">
+                <a href="#">
+                    {{ config('app.name') }}
+                </a>
+            </div>
+
+            <div class="right menu">
+                @guest
+                <a class="item" href="{{ route('login') }}">{{ __('Login') }}</a>
+                <a class="item" href="{{ route('register') }}">{{ __('Register') }}</a>
+
+                @else
+                    <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            {{ Auth::user()->name }} <span class="caret"></span>
+                        </a>
+
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                                onclick="event.preventDefault();
+                                                document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
+                            </a>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
+                        </div>
+                    </li>
+
+                @endguest
+            </div>
+
+        </div>
+    </div>
+
+        <div class="ui fluid container segment" style="height: 100%;">
+            @yield('content')
+        </div>
+    </div>
+</body>
+</html>
